03c3ef6f878e98e1736ab70be71863601385a586
Hoang Duy <lhd.duy1307@gmail.com>
2021-11-30 23:56:17 +0700

    Exercise: Dropdown/Custom Dropdown

--- selenium/webdriver/Topic_08_Dropdown.java
+++ selenium/webdriver/Topic_08_Dropdown.java
@@ -4,10 +4,15 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.server.handler.FindElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -17,38 +22,157 @@ public class Topic_08_Dropdown {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
 	Select select;
+	WebDriverWait explicitWait; // wait tường mình chờ cho trạng thái element
+	Topic_0405_Xpath_CSS topic_0405_Xpath_CSS = new Topic_0405_Xpath_CSS();
 
 	@BeforeClass
 	public void beforeClass() {
 		System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);// wait ngầm định chờ cho element dc tìm thấy
 		driver.manage().window().maximize();
+		explicitWait = new WebDriverWait(driver, 30); // chờ cho element dc load lên
 	}
-	
+
+	// @Test
+	public void TC_01_JQuery() {
+
+		driver.get("https://jqueryui.com/resources/demos/selectmenu/default.html");
+
+		selectItemInDropdown("//span[@id='number-button']", "//li[@class='ui-menu-item']", "19");
+		Assert.assertEquals(driver
+				.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text']")).getText(),
+				"19");
+		topic_0405_Xpath_CSS.SleepInSeconds(1);
+
+		selectItemInDropdown("//span[@id='number-button']", "//li[@class='ui-menu-item']", "1");
+		Assert.assertEquals(driver
+				.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text']")).getText(),
+				"1");
+		topic_0405_Xpath_CSS.SleepInSeconds(1);
+
+		selectItemInDropdown("//span[@id='number-button']", "//li[@class='ui-menu-item']", "5");
+		Assert.assertEquals(driver
+				.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text']")).getText(),
+				"5");
+		topic_0405_Xpath_CSS.SleepInSeconds(1);
+
+		selectItemInDropdown("//span[@id='number-button']", "//li[@class='ui-menu-item']", "10");
+		Assert.assertEquals(driver
+				.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text']")).getText(),
+				"10");
+
+		topic_0405_Xpath_CSS.SleepInSeconds(1);
+
+		selectItemInDropdown("//span[@id='number-button']", "//li[@class='ui-menu-item']", "15");
+		Assert.assertEquals(driver
+				.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text']")).getText(),
+				"15");
+		topic_0405_Xpath_CSS.SleepInSeconds(1);
+
+		selectItemInDropdown("//span[@id='number-button']", "//li[@class='ui-menu-item']", "7");
+		Assert.assertEquals(driver
+				.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text']")).getText(),
+				"7");
+		topic_0405_Xpath_CSS.SleepInSeconds(1);
+
+		selectItemInDropdown("//span[@id='number-button']", "//li[@class='ui-menu-item']", "17");
+		Assert.assertEquals(driver
+				.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text']")).getText(),
+				"17");
+		topic_0405_Xpath_CSS.SleepInSeconds(1);
+	}
+
+//	@Test
+	public void TC_02_ReactJS() {
+
+		driver.get("https://react.semantic-ui.com/maximize/dropdown-example-selection/");
+		// System.out.println(driver.findElement(By.xpath("//div[@class='divider default
+		// text']")).getText());
+
+		selectItemInDropdown("//div[@class='ui fluid selection dropdown']", "//span[@class='text']", "Christian");
+		driver.findElement(By.xpath("//div[@class='divider text']")).getText();
+		System.out.println(driver.findElement(By.xpath("//div[@class='divider text']")).getText());
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='divider text']")).getText(), "Christian");
+
+		topic_0405_Xpath_CSS.SleepInSeconds(1);
+		selectItemInDropdown("//div[@class='ui fluid selection dropdown']", "//span[@class='text']", "Jenny Hess");
+		System.out.println(driver.findElement(By.xpath("//div[@class='divider text']")).getText());
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='divider text']")).getText(), "Jenny Hess");
+
+		topic_0405_Xpath_CSS.SleepInSeconds(1);
+		selectItemInDropdown("//div[@class='ui fluid selection dropdown']", "//span[@class='text']",
+				"Stevie Feliciano");
+		System.out.println(driver.findElement(By.xpath("//div[@class='divider text']")).getText());
+		Assert.assertEquals(driver.findElement(By.xpath("//div[@class='divider text']")).getText(), "Stevie Feliciano");
+
+	}
+
 	@Test
-	public void TC_01() {
-		String country = "Vietnam";
-		driver.get("https://www.rode.com/wheretobuy");
-		select = new Select(driver.findElement(By.name("where_country")));
-		Assert.assertFalse(select.isMultiple()); // kiem tra dropdown ko phai la multi
-		select.selectByVisibleText(country);
-		Assert.assertEquals(select.getFirstSelectedOption().getText(),country);
-		//System.out.println(select.getFirstSelectedOption().getText());
-		driver.findElement(By.id("search_loc_submit")).click();
-		Assert.assertEquals(driver.findElement(By.xpath("//div[(text()=', Vietnam')]")).getText(),", "+country);
-		
-		List<WebElement> storeName = driver.findElements(By.xpath("//div[@id='search_results']//div[@class='store_name']"));
-		for (WebElement webElement : storeName) {
-			System.out.println(webElement.getText() + "\n");
-		}
-		
+	public void TC_03_VueJS() {
+
+		driver.get("https://mikerodham.github.io/vue-dropdowns/");
+		selectItemInDropdown("//li[@class='dropdown-toggle']", "//ul[@class='dropdown-menu']//a", "First Option");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='dropdown-toggle']")).getText(), "First Option");
+		topic_0405_Xpath_CSS.SleepInSeconds(1);
+
+		selectItemInDropdown("//li[@class='dropdown-toggle']", "//ul[@class='dropdown-menu']//a", "Second Option");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='dropdown-toggle']")).getText(), "Second Option");
+		topic_0405_Xpath_CSS.SleepInSeconds(1);
+
+		selectItemInDropdown("//li[@class='dropdown-toggle']", "//ul[@class='dropdown-menu']//a", "Third Option");
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='dropdown-toggle']")).getText(), "Third Option");
+		topic_0405_Xpath_CSS.SleepInSeconds(1);
+
 	}
+
+	public void selectItemInDropdown(String parentXpath, String childXpath, String expectedItem) {
+		driver.findElement(By.xpath(parentXpath)).click(); // click để mở các item con
+
+		// gọi hàm wait khác class
+		topic_0405_Xpath_CSS.SleepInSeconds(2);
+
+		// Chờ cho tất cả các item con dc hiển thị trong Dom trong x giây
+		explicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(childXpath)));
+
+		// Lấy hết tất cả các item
+		List<WebElement> childItems = driver.findElements(By.xpath(childXpath));
+		for (WebElement tempElement : childItems) {
+			// Kiểm tra đúng với expected
+			if (tempElement.getText().trim().equals(expectedItem)) {
+
+				if (tempElement.isDisplayed()) {
+					Actions ac = new Actions(driver);
+					ac.moveToElement(tempElement).perform();
+					ac.click(tempElement).perform();
+
+				} else {
+					JavascriptExecutor js = (JavascriptExecutor) driver;
+					js.executeScript("arguments[0].scrollIntoView(true);", tempElement);
+					Actions ac = new Actions(driver);
+					ac.moveToElement(tempElement).perform();
+					ac.click(tempElement).perform();
+
+				}
+				// System.out.println(childItems);
+//				System.out.println("" + tempElement.getText() + "\n");
+//				WebElement element = driver.findElement(By.xpath(childXpath));
+//				JavascriptExecutor js = (JavascriptExecutor) driver;
+//				js.executeScript("arguments[0].scrollIntoView(true);", element);
+				// ((JavascriptExecutor)
+				// driver).executeScript("arguments[0].scrollIntoView(true);", tempElement);
+//				explicitWait.until(ExpectedConditions.refreshed(tempElement, "class", "ui-state-active"));
+
+//			ac.moveToElement(tempElement).click().perform();// như vậy nhanh quá sẽ bị lỗi
+//				tempElement.click();
+				break;
+			}
+		}
+	}
+
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
 	}
 
-	
-	
 }
